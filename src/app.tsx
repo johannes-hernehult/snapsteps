1f5af9607c214585ea1a51e6a1c65b46f119daa7
Johannes Hernehult <jhernehult@gmail.com>
2026-04-14 20:27:20 +0200

    styling: Add reset.css, variable.css and remove styles in app.css

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,9 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import "./app.css";
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/app.css";
 
 export default function App() {
   return (
