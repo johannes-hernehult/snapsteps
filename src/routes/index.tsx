26ead04e73c0337c31d41749dcc5809dad630187
Johannes Hernehult <jhernehult@gmail.com>
2026-04-15 20:04:09 +0200

    Setup: Remove markup in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,6 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
