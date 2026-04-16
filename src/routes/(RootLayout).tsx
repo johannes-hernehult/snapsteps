a326e5564f211a1d3b819f5d795d512baf2138e7
Johannes Hernehult <jhernehult@gmail.com>
2026-04-16 19:48:33 +0200

    feat: remove guide.store.ts, add guide.context.tsx. Wire up with GuideList.tsx

--- src/routes/(RootLayout).tsx
+++ src/routes/(RootLayout).tsx
@@ -1,11 +1,12 @@
 import { RouteSectionProps } from "@solidjs/router";
+import { GuideProvider } from "~/lib/contexts/guide.context";
 
-export default function RootLayout(props: RouteSectionProps) {
+export default function Layout(props: RouteSectionProps) {
   return (
-    <>
+    <GuideProvider>
       <header>Logo</header>
       {props.children}
       <footer>Copyright {new Date().getFullYear()}</footer>
-    </>
+    </GuideProvider>
   );
 }
--- src/routes/(RootLayout)/index.tsx
+++ src/routes/(RootLayout)/index.tsx
@@ -1,10 +1,12 @@
 import { Title } from "@solidjs/meta";
+import GuideList from "~/components/guide-list/GuideList";
 
 export default function Home() {
   return (
     <main>
       <Title>SnapSteps - Landing page</Title>
       <h1>Hello SnapSteps</h1>
+      <GuideList />
     </main>
   );
 }
